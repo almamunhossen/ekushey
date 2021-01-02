b175eebcc3bedcb6d821c6d0e43f0007c57aa121
Al Mamun Hossen <almamunhossen@gmail.com>
2021-01-03 01:23:13 +0300

    first ekusye

---
+++ README.md
@@ -0,0 +1,3 @@
+# amhs
+# amhs
+# amhs
---
+++ js/sidenav.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").classList.remove("mmenuoff");
+  document.getElementById("mySidenav").classList.add("mmenuon");
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").classList.add("mmenuoff");
+  document.getElementById("mySidenav").classList.remove("mmenuon");
+}
